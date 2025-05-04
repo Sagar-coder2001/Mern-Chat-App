ee00b4dc0d89f02eee505e3cdc997e8e097a6f7a
sagar <shindesagar01677@gmail.com>
2025-05-03 23:37:36 -1200

    new

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "/api",
+  baseURL: import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "https://mern-chat-app-8l23.onrender.com/api",
   withCredentials: true,
 });

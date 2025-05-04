67d810b553a1f90417fffbbf849496fcecdc71ee
sagar <shindesagar01677@gmail.com>
2025-05-03 14:31:21 -1200

    nw

--- backend/index.js
+++ backend/index.js
@@ -20,7 +20,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: process.env.CLIENT_URL,
+    origin: "https://mern-chat-app-xi-ochre.vercel.app",
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
   })
